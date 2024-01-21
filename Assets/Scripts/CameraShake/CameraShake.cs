b9ae1f00e9d184dd7e74389a1fb8e9d2a588c8c9
Monishcreate <64581386+Monishcreate@users.noreply.github.com>
2024-01-21 11:29:10 +0530

    more more more

--- Assets/Scripts/CameraShake/CameraShake.cs
+++ Assets/Scripts/CameraShake/CameraShake.cs
@@ -7,7 +7,7 @@ public class CameraShake : MonoBehaviour
 {
     public static CameraShake instance;
     private CinemachineVirtualCamera Vcam;
-    private float shakeIntensity = 4f;
+    
     private float shakeTime = 0.1f;
 
 
@@ -21,7 +21,7 @@ public class CameraShake : MonoBehaviour
         Vcam = GetComponent<CinemachineVirtualCamera>();
     }
 
-    public void ShakeCamera()
+    public void ShakeCamera(float shakeIntensity)
     {
         CinemachineBasicMultiChannelPerlin _cbmcp = Vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         _cbmcp.m_AmplitudeGain = shakeIntensity;
