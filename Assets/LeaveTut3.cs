1b81ce1e015003c2e93ace7346e72184d8f40156
Monishcreate <64581386+Monishcreate@users.noreply.github.com>
2024-02-05 10:46:41 +0530

    hm

--- Assets/LeaveTut3.cs
+++ Assets/LeaveTut3.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LeaveTut3 : MonoBehaviour
 {
     // Start is called before the first frame update
-    float changeTime = 11f;
+    float changeTime = 17f;
 
 
     // Update is called once per frame
---
+++ Assets/TimeForRed.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeForRed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float changeTime = 7.7f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeTime -= Time.deltaTime;
+
+        if (changeTime <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
+
+
